1eb4bd765518d1a63443ac868affd3bcb2f6feb7
dishintejani <dishin22@gmail.com>
2018-06-06 10:25:44 -0400

    projects page added

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use((req, res, next) => {
    next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + "/public"));
 
@@ -47,6 +47,13 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
     });
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Project Page",
+    welcomeMessage: 'My PortFolio'
+  });
 });
 
 app.get("/bad", (req, res) => {
